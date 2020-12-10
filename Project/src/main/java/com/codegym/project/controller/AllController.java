117ddc97e48157663146472a9025d2933cc4c6d9
letanphuc1214 <tanphuc1214@gmail.com>
2020-12-10 16:43:31 +0700

    updateCRUDBanner

---
+++ Project/src/main/java/com/codegym/project/controller/AllController.java
@@ -0,0 +1,87 @@
+package com.codegym.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "")
+public class AllController {
+    @GetMapping(value = "/banners")
+    public ModelAndView listBanners(){
+        ModelAndView modelAndView=new ModelAndView("admin/Banner");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/blogs")
+    public ModelAndView listBlogs(){
+        ModelAndView modelAndView=new ModelAndView("admin/Blog");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/employees")
+    public ModelAndView listEmployees() {
+        ModelAndView modelAndView = new ModelAndView("admin/Employee");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/orders")
+    public ModelAndView listOrders(){
+        ModelAndView modelAndView=new ModelAndView("admin/Order");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/products")
+    public ModelAndView listProducts(){
+        ModelAndView modelAndView=new ModelAndView("/admin/Product");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/productDeleted")
+    public ModelAndView listProductsDeleted(){
+        ModelAndView modelAndView= new ModelAndView("/admin/ProductIsDeleted");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/rates")
+    public ModelAndView listRates(){
+        ModelAndView modelAndView=new ModelAndView("admin/Rate");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/ratesDeleted")
+    public ModelAndView listRatesDeleted(){
+        ModelAndView modelAndView=new ModelAndView("admin/RateIsDelete");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/typeProducts")
+    public ModelAndView listTypes(){
+        ModelAndView modelAndView=new ModelAndView("admin/ProductLine");
+        return modelAndView;
+    }
+    @GetMapping(value = "/typeProductsDeleted")
+    public ModelAndView listTypesDeleted(){
+        ModelAndView modelAndView=new ModelAndView("admin/ProductLineIsDelete");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/users")
+    public ModelAndView listUsers(){
+        ModelAndView modelAndView=new ModelAndView("admin/User");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/vendors")
+    public ModelAndView listVendors(){
+        ModelAndView modelAndView=new ModelAndView("admin/Vendor");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/vendorsDeleted")
+    public ModelAndView listVendorsDeleted(){
+        ModelAndView modelAndView=new ModelAndView("admin/VendorIsDelete");
+        return modelAndView;
+    }
+}
---
+++ Project/src/main/resources/static/js/ratesDeleted.js
@@ -0,0 +1,95 @@
+var rates = {} || rates;
+rates.intTable = function () {
+    $("#rates-datatables").DataTable({
+        ajax: {
+            url: 'http://localhost:8080/api/ratesDeleted/',
+            method: "GET",
+            datatype: "json",
+            dataSrc: ""
+        },
+        columns: [
+            {
+                data: "id", name: "ID", title: "ID", orderable: true
+            },
+            {
+                data: "name", name: "Name", title: "Name", orderable: true,
+            },
+            {
+                data: "email", name: "Email", title: "Email", sortable: false,
+                orderable: false,
+            },
+            {
+                data: "status", name: "Status", title: "Status", sortable: false,
+                orderable: false,
+            },
+            {
+                data: "dateAdd", name: "Date Add", title: "Date Add", sortable: false,
+                orderable: false
+            },
+            {
+                data: "dateDelete", name: "Date Delete", title: "Date Delete", sortable: false,
+                orderable: false
+            },
+            {
+                data: "id", name: "Action", title: "Action", sortable: false,
+                orderable: false, "render": function (data) {
+                    var str = "<div style='justify-content: center;text-align: center'><a href='javascript:' onclick='rates.get("+data+")' title='View' data-toggle=\"modal\" data-target=\"#modalAddEdit\" class='btn btn-warning'><i class=\"fa fa-eye\" aria-hidden=\"true\"></i></a> " +
+                        "<a href='javascript:' class='btn btn-danger' onclick='rates.delete("+data+")'><i class=\"ti-trash\" title=\"Delete\"></a></div>"
+                    return str;
+                }
+            }
+        ],
+    });
+};
+
+rates.delete = function (id) {
+    bootbox.confirm({
+        message: "Do you want to permanently delete the Rate",
+        buttons: {
+            confirm: {
+                label: 'Yes',
+                className: 'btn-success'
+            },
+            cancel: {
+                label: 'No',
+                className: 'btn-danger'
+            }
+        },
+        callback: function (result) {
+            if (result) {
+                $.ajax({
+                    url: "http://localhost:8080/api/rateDeleted/" + id,
+                    method: "DELETE",
+                    dataType: "json",
+                    success: function () {
+                        $("#rates-datatables").DataTable().ajax.reload();
+                        toastr.info('Rate has been deleted successfully', 'INFORMATION:')
+                    },
+                    error:function (jqXHR,exception){
+                        toastr.error('Error!! Rate not has been delete', 'INFORMATION:')
+                    }
+                });
+            }
+        }
+    });
+};
+
+rates.get = function (id) {
+    $.ajax({
+        url: "http://localhost:8080/api/findRateDeleted/" + id,
+        method: "GET",
+        dataType: "json",
+        success: function (data) {
+            $('#formAddEdit')[0].reset();
+            $('#modalTitle').html("View rate");
+            $('#name').val(data.name);
+            $('#email').val(data.email);
+            $('#content').val(data.content);
+            $('#dateAdd').val(data.dateAdd);
+            $('#modalAddEdit').modal('show');
+        }
+    });
+};
+$(document).ready(function () {
+    rates.intTable();
+});
